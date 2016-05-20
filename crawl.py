50156034b04d14e08adc958ec7ba48a545c5ed48
thepcn3rd <ltrappett@gmail.com>
2016-05-19 22:33:40 -0600

    Updated crawl.py to include showing the domain that is being crawled...

--- crawl.py
+++ crawl.py
@@ -4,10 +4,12 @@ import cmd
 import requests
 import re
 
+
 # Global Variables
 
 #emailRE = re.compile("[A-Za-z0-9-_\.]+@[A-Za-z0-9]+\.[a-zA-Z]{2,5}")
-emailRE = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+#emailRE = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+emailRE = re.compile("(?:[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+)")
 #urlRE = re.compile("(?:http\:\/\/|https\:\/\/|)[A-Za-z0-9-_\.]+\.[a-zA-Z]{2,5}")
 #urlRE = re.compile("(?:http\:\/\/|https\:\/\/|)(?:[a-zA-Z0-9-_\/]+|)[A-Za-z0-9-_\.]+\.[a-zA-Z]{2,5}")
 #urlRE = re.compile(".*((href=|src=)(\x22|')(?P<url>[a-zA-Z0-9-_\.\/]+)(\x22|'))|((?P<urlFull>(http:\/\/|https:\/\/)[a-zA-Z0-9-_\.]{5,})).*")
@@ -16,61 +18,87 @@ urlRE = re.compile("((href=|src=)(\x22|'|)(?P<url>(http:\/\/|https:\/\/|)[a-zA-Z
 # (?<url>(http:\/\/|https:\/\/)[a-zA-Z0-9-_\.]{5,}) (Standard URL Syntax)
 # (href=|src=)("|')(?<url>[a-zA-Z0-9-_\.\/]+)("|') (Pulling pages out from between tags)
 domainRE = re.compile("[a-zA-Z0-9-_\.]")
+extractDomainRE = re.compile("(https?:\/\/|^)(?P<domain>[a-zA-Z0-9-_\.]+)(\/|)")
 listEmails = []
 listURLs = []
 htmlContent = ""
+secureSite = 'N'
 
 
 def crawl(url, domain):
-    global listEmails, listURLs, urlRE, htmlContent
+    global listEmails, listURLs, urlRE, htmlContent, secureSite
     listCurrentEmails = []
     listCurrentPages = []
-    page = requests.get(url)
-    htmlContent = page.content
-    for line in page.content.split('\n'):
-        if urlRE.search(line) is not None:
-            webPage = urlRE.search(line)
-            if webPage.group('url') is not None:
-                if not webPage.group('url') in listCurrentPages and not webPage.group('url') in url:
-                    listCurrentPages.append(webPage.group('url'))
-            if webPage.group('urlFull') is not None:
-                if not webPage.group('urlFull') in listCurrentPages and not webPage.group('urlFull') in url:
-                    listCurrentPages.append(webPage.group('urlFull'))
-        if emailRE.search(line) is not None:
-            email = emailRE.search(line)
-            if not email.group(0) in listCurrentEmails:
-                listCurrentEmails.append(email.group(0))
-    print "Pages Collected"
-    print "---------------"
-    for i in range(0, len(listCurrentPages)):
-        print listCurrentPages[i]
-        if not "http" in listCurrentPages[i]:
-            # If the URL begins with a slash remove it to avoid double slashes in a URL
-            if listCurrentPages[i][:1] == "/":
-                listCurrentPages[i] = listCurrentPages[i][1:]
-            newURL = "http://" + domain + "/" + listCurrentPages[i]
-            if not newURL in listURLs:
-                listURLs.append(newURL)
-        else:
-            newURL = listCurrentPages[i]
-            if not newURL in listURLs:
-                listURLs.append(newURL)
-    print
-    print "Emails Collected from this Page"
-    print "-------------------------------"
-    for j in range(0, len(listCurrentEmails)):
-        print listCurrentEmails[j]
-        if not listCurrentEmails[j] in listEmails:
-            listEmails.append(listCurrentEmails[j])
-    print
-    print "Current URL: " + url
-    print
-    raw_input("Press any key to continue...")
-    print
+    try:
+        page = requests.get(url)
+    except:
+        page = None
+    if not page == None:
+        htmlContent = page.content
+        for line in page.content.split('\n'):
+            if urlRE.search(line) is not None:
+                webPage = urlRE.search(line)
+                if webPage.group('url') is not None:
+                    if not webPage.group('url') in listCurrentPages and not webPage.group('url') in url:
+                        listCurrentPages.append(webPage.group('url'))
+                if webPage.group('urlFull') is not None:
+                    if not webPage.group('urlFull') in listCurrentPages and not webPage.group('urlFull') in url:
+                        listCurrentPages.append(webPage.group('urlFull'))
+            if emailRE.search(line) is not None:
+                email = emailRE.search(line)
+                if not email.group(0) in listCurrentEmails:
+                    listCurrentEmails.append(email.group(0))
+        print "New Pages Collected"
+        print "-------------------"
+        for i in range(0, len(listCurrentPages)):
+            #print listCurrentPages[i]
+            if not "http" in listCurrentPages[i]:
+                # If the URL begins with a slash remove it to avoid double slashes in a URL
+                while True:
+                    if listCurrentPages[i][:1] == "/":
+                        listCurrentPages[i] = listCurrentPages[i][1:]
+                    else:
+                        break
+                if secureSite == 'Y':
+                    fullURL = "https://" + domain + "/" + listCurrentPages[i]
+                    newURL = "NV|" + "https://" + domain + "/" + listCurrentPages[i]
+                else:
+                    fullURL = "http://" + domain + "/" + listCurrentPages[i]
+                    newURL = "NV|" + "http://" + domain + "/" + listCurrentPages[i]
+                if not ("NV|" + fullURL) in listURLs and not ("V|" + fullURL) in listURLs:
+                    listURLs.append(newURL)
+                    print fullURL
+            else:
+                fullURL = listCurrentPages[i]
+                newURL = "NV|" + listCurrentPages[i]
+                if not ("NV|" + fullURL) in listURLs and not ("V|" + fullURL) in listURLs:
+                    listURLs.append(newURL)
+                    print fullURL
+        print
+        print "New Emails Collected from this Page"
+        print "-----------------------------------"
+        for j in range(0, len(listCurrentEmails)):
+            #print listCurrentEmails[j]
+            if not listCurrentEmails[j] in listEmails:
+                listEmails.append(listCurrentEmails[j])
+                print listCurrentEmails[j]
+        print
+        print "Current URL: " + url
+        print "Current Domain: " + domain
+        print
+        raw_input("Press enter key to continue...")
+        print
+    else:
+        print
+        print "Error Crawling URL: " + url
+        print
+        raw_input("Press enter key to continue...")
+        print
 
 
 class webCrawler(cmd.Cmd):
     def __init__(self):
+        global secureSite
         print
         print "This web crawler is designed to allow you to manually crawl through a site."
         print "As it crawls it will extract urls and email addresses based on regular expressions."
@@ -82,12 +110,13 @@ class webCrawler(cmd.Cmd):
         secureURL = raw_input("Is the domain a secure site? ")
         if secureURL == 'y' or secureURL == 'Y':
             self.url = "https://" + self.domain
+            secureSite='Y'
         else:
             self.url = "http://" + self.domain
+            secureSite='N'
         print
         crawl(self.url, self.domain)
-        print
-        print "To begin crawling manually type: selectURL"
+        self.do_selectURL(self)
         print
         cmd.Cmd.__init__(self)
         self.prompt = "#> "
@@ -102,23 +131,27 @@ class webCrawler(cmd.Cmd):
     def do_recrawl(self, input):
         'Recrawls the current URL'
         crawl(self.url, self.domain)
+        self.do_selectURL(self)
         return
 
     def do_crawl(self, input):
         'Recrawls the current URL'
         crawl(self.url, self.domain)
+        self.do_selectURL(self)
         return
 
     def do_changeDomain(self, newDomain):
         'Change the current URL manually'
-        global domainRE
+        global domainRE, secureSite
         if domainRE.match(newDomain):
             self.domain = newDomain.lower()
             secureURL = raw_input("Is the domain a secure site? ")
             if secureURL == 'y' or secureURL == 'Y':
                 self.url = "https://" + self.domain
+                secureSite = 'Y'
             else:
                 self.url = "http://" + self.domain
+                secureSite = 'N'
         else:
             print "Incorrect format based on the regular expression provided."
             print "Syntax: changeURL http://scriptkitty.work"
@@ -127,17 +160,25 @@ class webCrawler(cmd.Cmd):
 
     def do_selectURL(self, input):
         'Select a URL from the list collected that has not been crawled...'
-        global listURLs, htmlContent
+        global listURLs, htmlContent, extractDomainRE
         while True:
             currentIDs = []
             print
             print "Select URL to Crawl Next"
             print "------------------------"
             for i in range(0, len(listURLs)):
-                print str(i) + ": " + listURLs[i]
+                items = listURLs[i].split("|")
+                if items[1] == self.url:
+                    listURLs[i] = "V|" + items[1]
+                    items = listURLs[i].split("|")
+                if items[0] == 'NV':
+                    print str(i) + ": [Not Visited] " + items[1]
+                elif items[0] == 'V':
+                    print str(i) + ": [Visited] " + items[1]
                 currentIDs.append(str(i))
             print
             print "Current URL: " + self.url
+            print "Current Domain: " + self.domain
             print
             print "C. Crawl Current URL"
             print "D. Display HTML Content of Current URL"
@@ -146,20 +187,26 @@ class webCrawler(cmd.Cmd):
             print "Q. Stop Crawling Loop"
             selection = raw_input("Select the URL by the preceeding number: ")
             if selection in currentIDs:
-                self.url = listURLs[int(selection)]
+                items = listURLs[int(selection)].split("|")
+                self.url = items[1]
+                if domainRE.search(self.url) is not None:
+                    newDomain = extractDomainRE.search(self.url)
+                    if newDomain.group('domain') is not None:
+                        self.domain = newDomain.group('domain')
                 crawl(self.url, self.domain)
             elif selection == "c" or selection == "C":
                 crawl(self.url, self.domain)
             elif selection == "d" or selection == "D":
                 print htmlContent
                 print
-                raw_input("Press any key to continue...")
+                raw_input("Press enter key to continue...")
             elif selection == "e" or selection == "E":
                 editSelection = raw_input("URL to Edit (Select by preceeding number): ")
                 if editSelection in currentIDs:
-                    print "Editing the following URL: " + listURLs[int(editSelection)]
+                    items = listURLs[int(editSelection)].split("|")
+                    print "Editing the following URL: " + items[1]
                     newURL = raw_input("New URL: ")
-                    listURLs[int(editSelection)] = newURL
+                    listURLs[int(editSelection)] = items[0] + "|" + newURL
                 else:
                     print "Invalid URL Selection to Edit"
             elif selection == "r" or selection == "R":
@@ -175,6 +222,7 @@ class webCrawler(cmd.Cmd):
     def do_displayURL(self, input):
         'Display the current URL'
         print "Current URL: " + self.url
+        print "Current Domain: " + self.domain
         return
 
     def do_flushEmailCache(self, input):
@@ -212,7 +260,11 @@ class webCrawler(cmd.Cmd):
                 fPath = "output/" + filename
                 f = open(fPath, "w")
                 for i in range(0, len(listURLs)):
-                    f.write(listURLs[i] + "\n")
+                    items = listURLs[i].split("|")
+                    if items[0] == "V":
+                        f.write("[Visited] " + items[1] + "\n")
+                    elif items[0] == "NV":
+                        f.write("[Not_Visited] " + items[1] + "\n")
                 f.close()
                 print "Saved the output to the following file: " + fPath
         else:
@@ -237,7 +289,11 @@ class webCrawler(cmd.Cmd):
         print "URL Cache"
         print "---------"
         for i in range(0, len(listURLs)):
-            print listURLs[i]
+            items = listURLs[i].split("|")
+            if items[0] == "V":
+                print "[Visited] " + items[1]
+            else:
+                print "[Not Visited] " + items[1]
         print
         return
 
